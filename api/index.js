50f62aa1a074211ea2158ec1540c933a7ca6fb0a
iamjustcurious <durgadasdhoke@gmail.com>
2024-06-17 11:24:30 +0530

    models created

--- api/index.js
+++ api/index.js
@@ -1,10 +1,24 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log('mogoose working properly');
+}).catch((err)=>{
+    console.log(err);
+});
+
 
 const app = express();
 
-app.listen(3000,(req,res)=>{
+app.listen(process.env.PORT,(req,res)=>{
     console.log("app is running on port number 3000");
 });
+
+// mongodb+srv://durgadasdhoke:<password>@cluster0.odc8qqo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 
 // app.get('/',(req,res)=>{
 //     res.json("Hello welcome");
